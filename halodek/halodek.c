74c74b1b0819754e92450f7221d8ca93b2eb4670
Dwiko Dany Rananta <dwiko.dev@gmail.com>
2025-08-28 22:34:43 +0700

    ln: format specifier

--- halodek/halodek.c
+++ halodek/halodek.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // char nama = "Dwiko";
-    // int umur = scanf("%s", &nama);
+    char nama[20];
+    int tahunLahir, umur;
 
-    printf("Hello World\n Line kedua\n \t");
+    printf("Ketikan nama anda : \n");
+    scanf(" %s", &nama);
+
+    printf("Ketikan tahun lahir anda : \n");
+    if (scanf("%d", &tahunLahir) > 0) {
+        umur = 2025 - tahunLahir;
+        printf("Halo nama saya %c, umur saya %d tahun", nama, umur);
+    } else {
+        printf("Silahkan masukan tahun lahir dengan benar \n");
+    }
 
     return 0;
 }
